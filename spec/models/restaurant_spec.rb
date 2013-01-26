27f00354ef250e6630218a9a34d2e8a3fb6cee64
Errin Larsen <elarsen@insightcards.com>
2013-01-26 13:02:16 -0600

    Add some association testing

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,4 +4,22 @@ describe Restaurant do
   it { should respond_to :name }
   it { should respond_to :entries }
   it { should respond_to :users }
+
+  before(:each) do
+    @restaurant = Restaurant.new
+  end
+
+  it "can be associated with an Entry" do
+    @restaurant.entries << Entry.new
+    @restaurant.should have(:no).errors_on(:entries)
+  end
+
+  it "can be associated with a User" do
+    some_user = User.new
+    some_entry = Entry.new
+    some_entry.user = some_user
+    @restaurant.entries << some_entry
+
+    @restaurant.should have(:no).errors_on(:users)
+  end
 end
