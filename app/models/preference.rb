0af7946eb99fd3936fab2e15c37c62ad303b9bd3
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-01-26 21:50:58 -0600

    Trying to get Preference.new(:like => true) to work

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,6 +1,11 @@
 class Preference < ActiveRecord::Base
+  attr_accessible :like, :user_id, :restaurant_id
   belongs_to :user
   belongs_to :restaurant
+
+  def self.likes
+  	#self.all.map { |pref| pref.likes }
+  end
 
   def self.evaluate
     # stuff
--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -16,4 +16,17 @@ describe Preference do
       @pref.should have(:no).errors_on(:user)
     end
   end
+
+  describe "Like Method" do
+    it "Likes should default to false" do
+      @pref.like.should be_false
+    end
+
+    it "Likes should" do
+      Preference.likes.all? { |p| p.like == true }.should be_true
+    end
+
+
+
+  end
 end
